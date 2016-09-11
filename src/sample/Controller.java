57c5d59f2616e953e87c1161956d1df08e889e77
Denis Misuyra <script972@gmail.com>
2016-09-12 00:39:47 +0300

    first commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,106 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.lang.Math.cos;
+
+public class Controller implements Initializable {
+    double minX=-10;
+    double maxX=10;
+    double minY=-10;
+    double maxY=10;
+
+    @FXML
+    private Pane centerPane;
+    @FXML
+    private Canvas canvas;
+    @FXML
+    private TextField min1X;
+    @FXML
+    private TextField max1X;
+    @FXML
+    private TextField min1Y;
+    @FXML
+    private TextField max1Y;
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        canvas.widthProperty().bind(centerPane.widthProperty());
+        canvas.heightProperty().bind(centerPane.heightProperty());
+        canvas.widthProperty().addListener(e->draw());
+        canvas.heightProperty().addListener(e->draw());
+    }
+
+
+
+
+
+
+    public void draw() {
+        if(!min1X.getText().isEmpty()) {
+            this.minX = Double.parseDouble(min1X.getText());
+            this.maxX = Double.parseDouble(max1X.getText());
+            this.minY = Double.parseDouble(min1Y.getText());
+            this.maxY = Double.parseDouble(max1Y.getText());
+        }
+
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.AQUA);
+        gc.fillRect(0,0,canvas.getWidth(),canvas.getHeight());
+        gc.strokeLine(toScreenX(minX), toScreenY(0), toScreenX(maxX), toScreenY(0));
+        gc.strokeLine(toScreenX(0),toScreenY(maxY),toScreenX(0),toScreenY(minY));
+        List<Point> points = tabulation();
+        gc.beginPath();
+        gc.moveTo(toScreenX(points.get(0).getX()), toScreenY(points.get(0).getY()));
+        for (int i=1; i<points.size(); i++) {
+            gc.lineTo(toScreenX(points.get(i).getX()), toScreenY(points.get(i).getY()));
+        }
+        gc.stroke();
+    }
+
+    private List<Point> tabulation() {
+        return IntStream
+                .range(0, (int) canvas.getWidth())
+                .mapToDouble(this::toWorldX)
+                .mapToObj(x->new Point(x, f(x)))
+                .collect(Collectors.toList());
+    }
+
+
+
+
+    private int toScreenX(double x) {
+        return (int) (canvas.getWidth() * (x - minX) / (maxX - minX));
+    }
+
+    private int toScreenY(double y) {
+        return (int) (canvas.getHeight() * (1 - (y - minY) / (maxY - minY)));
+    }
+
+    private double toWorldX(int xs) {
+        return 1.0 * xs / canvas.getWidth() * (maxX - minX) + minX;
+    }
+
+    private double toWorldY(int ys) {
+        return (1.0 * ys - canvas.getHeight()) / (-canvas.getHeight()) * (maxY - minY) + minY;
+    }
+
+    private double f(double x) {
+        return cos(x+0.1)/(x*x+1);
+    }
+}
---
+++ src/sample/Point.java
@@ -0,0 +1,31 @@
+package sample;
+
+/**
+ * Created by script972 on 11.09.2016.
+ */
+public class Point {
+    private double x;
+    private double y;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public Point(double x, double y) {
+
+        this.x = x;
+        this.y = y;
+    }
+}
